375594ebbf927f0548f612e621c165558fcb04f0
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-04 21:42:46 +0300

    Fix errors raised by rubocop

--- app/models/bond.rb
+++ app/models/bond.rb
@@ -1,13 +1,13 @@
 class Bond < ApplicationRecord
-    STATES = [
-        FOLLOWING = 'following',
-        REQUESTING = 'requesting'
-    ].freeze
-    
-    scope :following, -> { where(state: FOLLOWING) }
-    scope :requesting, -> { where(state: REQUESTING) }
+  STATES = [
+    FOLLOWING = 'following'.freeze,
+    REQUESTING = 'requesting'.freeze
+  ].freeze
 
-    validates :state, inclusion: { in: STATES }
-    belongs_to :user
-    belongs_to :friend, class_name: 'User'
+  scope :following, -> { where(state: FOLLOWING) }
+  scope :requesting, -> { where(state: REQUESTING) }
+
+  validates :state, inclusion: { in: STATES }
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
 end

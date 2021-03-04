0867b0cecf93eb87351a906a0c2e2d1b3a792641
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-04 14:20:11 +0300

    add Bond model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_165841) do
+ActiveRecord::Schema.define(version: 2021_03_04_110747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bonds", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.string "state", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "friend_id"], name: "index_bonds_on_user_id_and_friend_id", unique: true
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -54,4 +63,6 @@ ActiveRecord::Schema.define(version: 2020_02_04_165841) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bonds", "users"
+  add_foreign_key "bonds", "users", column: "friend_id"
 end
---
+++ spec/factories/bonds.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :bond do
+    user_id { 1 }
+    friend_id { 1 }
+  end
+end

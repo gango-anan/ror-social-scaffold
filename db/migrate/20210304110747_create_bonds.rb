bd775496efe34d2160e70154c4baf796602144af
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-04 14:20:11 +0300

    Add Bond model

---
+++ db/migrate/20210304110747_create_bonds.rb
@@ -0,0 +1,15 @@
+class CreateBonds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bonds do |t|
+      t.integer :user_id, null: false
+      t.integer :friend_id, null: false
+      t.string :state, null: false
+
+      t.timestamps
+    end
+
+    add_index :bonds, [:user_id, :friend_id], unique: true
+    add_foreign_key :bonds, :users, column: :user_id
+    add_foreign_key :bonds, :users, column: :friend_id
+  end
+end

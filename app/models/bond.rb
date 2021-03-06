ceb6f13311768b67104b0d68a9879d22ce22b300
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-06 21:03:25 +0300

    fix user model, bond model and add specs for user and bond models

--- app/models/bond.rb
+++ app/models/bond.rb
@@ -1,13 +1,7 @@
 class Bond < ApplicationRecord
-  STATES = [
-    FOLLOWING = 'following'.freeze,
-    REQUESTING = 'requesting'.freeze
-  ].freeze
+  scope :confirmed_friendship, -> { where(state: true) }
+  scope :pending_friendship, -> { where(state: false) }
 
-  scope :following, -> { where(state: FOLLOWING) }
-  scope :requesting, -> { where(state: REQUESTING) }
-
-  validates :state, inclusion: { in: STATES }
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 end

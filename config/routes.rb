fe44c720b44a048bb9183e115bd5a5085ffa519b
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-09 01:52:43 +0300

    Fix typo in friends view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
-
+  get 'users/friends'
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]

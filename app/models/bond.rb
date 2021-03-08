16a88a194e578b9bd9040b99dfadeb3e40a1a47b
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-08 19:06:57 +0300

    Refactor User model code to fix associations for direct_friendship

--- app/models/bond.rb
+++ app/models/bond.rb
@@ -1,7 +1,4 @@
 class Bond < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-
-  scope :confirmed_friendship, -> { where(state: true) }
-  scope :pending_friendship, -> { where(state: false) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,27 +9,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :bonds
-  has_many :friends, -> { Bond.confirmed_friendship }, through: :bonds, source: :friend
-  has_many :friend_requests, -> { Bond.pending_friendship }, through: :bonds, source: :friend
-  has_many :inward_bonds, class_name: 'Bond', foreign_key: :friend_id
-  has_many :inward_friends, -> { Bond.confirmed_friendship }, through: :inward_bonds, source: :user
+  default_scope -> { order(:id)}
 
-  def friends
-    @bonds = Bond.all
-    @direct_friends = @bonds.map{ |bond| bond.friend if bond.state }
-    @inward_friends = @bonds.map{ |bond| bond.user if bond.state }
-    (@direct_friends + @inward_friends).compact
-  end
-
-  def user_friends(user)
-    bonds = Bond.all
-    direct_friends = user.bonds.map{ |bond| bond.friend if bond.state }
-    indirect_friends = bonds.where(friend_id: user.id).map{ |bond| bond.user if bond.state }
-    (direct_friends + indirect_friends).compact
-  end
-
-  def check_friend(user1,user2)
-    user_friends(user1).include?(user2)
-  end
+  has_many :direct_friendships, class_name: 'Bond', foreign_key: 'user_id', dependent: :destroy
+  has_many :direct_friends, through: :direct_friendships, source: :friend
 end

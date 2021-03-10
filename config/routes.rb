c3d4cc8f816127f1dae90d6fca54ec7300266418
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-10 15:55:58 +0300

    Revert code

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,12 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
-  get 'users/friends'
-  get 'users/incoming_requests'
-  get 'users/outgoing_requests'
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-  resources :bonds, only: [:create, :update, :destroy]
+  resources :bonds
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

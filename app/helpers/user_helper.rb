49a5006668c443d1be9e1b3a1b2d7e4820d67c50
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-11 01:30:54 +0300

    Fix errors raised by rubocop

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,27 +1,27 @@
 module UserHelper
-    def manage_invite_requests(user)
-        return if current_user == user
-        return render 'users/invite' unless current_user.check_if_my_friend(user)
+  def manage_invite_requests(user)
+    return if current_user == user
+    return render 'users/invite' unless current_user.check_if_my_friend(user)
 
-        'Friend'
-    end
-
-    def manage_create_friendship(user)
-        return if current_user == user
-        return render 'users/create_bond' unless current_user.check_if_my_friend(user)
-
-        'Friend'
-    end
-
-    def manage_requests_to_me(user)
-         return unless current_user.pending_friends.include?(user)
-
-        render 'users/accept'
-    end
-
-    def manage_requests_to_others(user)
-        return unless current_user.unconfirmed_sent_requests.include?(user)
-
-        'pending confirmation'
-    end
+    'Friend'
   end
+
+  def manage_create_friendship(user)
+    return if current_user == user
+    return render 'users/create_bond' unless current_user.check_if_my_friend(user)
+
+    'Friend'
+  end
+
+  def manage_requests_to_me(user)
+    return unless current_user.pending_friends.include?(user)
+
+    render 'users/accept'
+  end
+
+  def manage_requests_to_others(user)
+    return unless current_user.unconfirmed_sent_requests.include?(user)
+
+    'pending confirmation'
+  end
+end

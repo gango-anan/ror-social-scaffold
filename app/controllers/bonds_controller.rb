6189052bb390051b301f1d534ddf3ab2a1f46e74
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-10 11:34:40 +0300

    refactor code base to remove unused code

--- app/controllers/bonds_controller.rb
+++ app/controllers/bonds_controller.rb
@@ -9,4 +9,16 @@ class BondsController < ApplicationController
     current_user.invite_to_friendship(user)
     redirect_to users_path
   end
+
+  def update
+    user = User.find(params[:user_id])
+    current_user.confirm_friendship(user)
+    redirect_to users_path
+  end
+
+  def destroy
+    user = Bond.find(params[:id]).friend
+    current_user.reject_friendship_request(user)
+    redirect_to current_user
+  end
 end

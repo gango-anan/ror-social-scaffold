ca4cd259c0e2fb2fb4a5eb94ebf414cb9e35c4d1
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-11 13:27:10 +0300

    Add some unconfirmed_requests utility method to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe User, type: :model do
       Bond.create user: user, friend: friend2, state: true
       Bond.create user: user, friend: friend3, state: false
 
-      expect(user.friends).to include(friend1, friend2)
-      expect(user.friends).not_to include(friend3)
+      expect(user.confirmed_friends).to include(friend1, friend2)
+      expect(user.confirmed_friends).not_to include(friend3)
     end
 
     it "can list all of the user's pending friendship requests" do
@@ -28,8 +28,8 @@ RSpec.describe User, type: :model do
       Bond.create user: user, friend: friend3, state: false
       Bond.create user: user, friend: friend4, state: false
 
-      expect(user.friend_requests).to include(friend3, friend4)
-      expect(user.friend_requests).not_to include(friend1, friend2)
+      expect(user.unconfirmed_requests).to include(friend3, friend4)
+      expect(user.unconfirmed_requests).not_to include(friend1, friend2)
     end
   end
 end

c550fa37a276b8fe181b66b483d139d258e20c9a
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-11 23:27:52 +0300

    refactor code to display invitation requests correctly

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-  resources :bonds, except: :edit
+  resources :bonds, except: [:edit, :show]
   get 'bonds/invitations'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

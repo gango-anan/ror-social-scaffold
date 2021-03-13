8ec70b38b8298a77fb2e7e334d250d479f12da3a
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-13 21:53:21 +0300

    Fix errors raised by rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ end
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
-  config.extend ControllerMacros, :type => :controller
+  config.extend ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 

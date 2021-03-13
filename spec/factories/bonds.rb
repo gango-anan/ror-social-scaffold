5aa2db9824948b879f35ea091024f7bd91559065
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-13 20:43:41 +0300

    complete adding bonds controller specs

--- spec/factories/bonds.rb
+++ spec/factories/bonds.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :bond do
-    user_id { 1 }
-    friend_id { 1 }
-    state { flase }
+    user_id { create(:user).id }
+    friend_id { create(:user).id }
+    state { false }
   end
 end

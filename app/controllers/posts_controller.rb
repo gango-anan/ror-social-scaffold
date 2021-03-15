65b33f6bbd4809ebea1b14560052a7c1afd4246f
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-15 21:50:55 +0300

    Add the functionality of displaying the user and friends posts only.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
   def timeline_posts
     timeline_post ||= Post.all.ordered_by_most_recent.includes(:user)
-    timeline_post.where(user: (current_user.confirmed_friends.to_a << current_user))
+    timeline_post = current_user.friends_and_own_posts
   end
 
   def post_params

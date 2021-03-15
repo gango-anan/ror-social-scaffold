c63f2823cb1244fee93e9bf6754f7f311213099a
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-15 23:01:28 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :bonds
+  end
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-  resources :bonds, except: [:edit, :show]
-  get 'bonds/invitations'
-  post 'invite' => 'bonds#create', as: 'invite'
+  resources :bonds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

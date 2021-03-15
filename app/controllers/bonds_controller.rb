50bf97c8b611011588cbb8edc0392bc281597393
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-15 23:01:58 +0300

    Remove unused code

--- app/controllers/bonds_controller.rb
+++ app/controllers/bonds_controller.rb
@@ -2,37 +2,17 @@ class BondsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @bonds = current_user.my_friendships
-    @friends = current_user.my_friends
-    @confirmed_friends = current_user.confirmed_friends
   end
 
-  def invitations
+  def show
   end
-
-  def new
-    @bond = Bond.new
-  end
-
-  def show; end
 
   def create
-    user = User.find(params[:friend_id])
-    current_user.invite_to_friendship(user)
-    redirect_to bonds_invitations_path, notice: 'Friendship Invitation Sent Succesfully.'
   end
 
   def update
-    bond = Bond.find(params[:id])
-    user = bond.user
-    new_user = current_user
-    bond.confirm_friendship(user, new_user)
-    redirect_to bonds_path
   end
 
   def destroy
-    bond = Bond.find(params[:id])
-    bond.destroy
-    redirect_to bonds_invitations_path
   end
 end

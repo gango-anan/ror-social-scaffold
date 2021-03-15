aac67085e37d778e962b3de5e6d69750b4c7f517
Galiwango Ananiya <galiwango101@gmail.com>
2021-03-15 03:48:34 +0300

    Create confirm_friendship method in bond Model

--- app/models/bond.rb
+++ app/models/bond.rb
@@ -1,4 +1,10 @@
 class Bond < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+
+  def confirm_friendship(user, new_user)
+    self.update_attributes(state: true)
+    Bond.create!(user_id: new_user.id, friend_id: user.id, state: true)
+  end
 end
